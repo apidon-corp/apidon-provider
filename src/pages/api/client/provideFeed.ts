0be0957a3b87040dc4dc0c18879a0264a2d64c93
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-06-10 17:04:01 +0300

    JSON Algorithm is disabled temporarily.

--- src/pages/api/client/provideFeed.ts
+++ src/pages/api/client/provideFeed.ts
@@ -26,31 +26,35 @@ export default async function handler(
   if (!providerDocSnapshot.exists)
     return res.status(422).json({ error: "Invalid Proivder Name" });
 
-  const algorithm = providerDocSnapshot.data()?.algorithm;
+  // const algorithm = providerDocSnapshot.data()?.algorithm;
 
-  if (!algorithm)
-    return res
-      .status(500)
-      .json({ error: `${provider} has no algorithm in it self` });
+  // if (!algorithm)
+  //   return res
+  //     .status(500)
+  //     .json({ error: `${provider} has no algorithm in it self` });
 
-  let algorithmJSON: { mode: string };
-  try {
-    algorithmJSON = JSON.parse(algorithm);
-  } catch (error) {
-    console.error("Error while parsing algorithm as JSON.", error);
-    return res
-      .status(500)
-      .json({ error: "Algorithm couldn't be parsed as JSON" });
-  }
+  // let algorithmJSON: { mode: string };
+  // try {
+  //   algorithmJSON = JSON.parse(algorithm);
+  // } catch (error) {
+  //   console.error("Error while parsing algorithm as JSON.", error);
+  //   return res
+  //     .status(500)
+  //     .json({ error: "Algorithm couldn't be parsed as JSON" });
+  // }
 
-  const algorithmMode = algorithmJSON.mode;
+  // const algorithmMode = algorithmJSON.mode;
 
   let postDocPathArray: string[] = [];
-  if (algorithmMode === "fun") {
+  if (provider === "SmartFeed") {
     postDocPathArray = [
       "users/cringememes/posts/9762879caf1d46aaa837602a218b8c84",
     ];
-  } else if (algorithmMode === "serious") {
+  } else if (provider === "AdBoost") {
+    postDocPathArray = [
+      "users/voiceofrize/posts/3ef548095acd43749fdbd288d2afed1f",
+    ];
+  } else if (provider === "ArtisticArea") {
     postDocPathArray = [
       "users/voiceofrize/posts/3ef548095acd43749fdbd288d2afed1f",
     ];
