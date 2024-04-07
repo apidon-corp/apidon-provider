346c27e73858833a26bd8e8278e1a611ba25a99a
aboveStars <yunuskorkmazis384@outlook.com>
2024-04-08 01:12:07 +0300

    Prepared for Mercury Launch.

--- src/pages/api/user/billing/createPaymentRule.ts
+++ src/pages/api/user/billing/createPaymentRule.ts
@@ -12,7 +12,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export const config = {
   runtime: "nodejs",
-  maxDuration: 60,
+  maxDuration: 120,
 };
 
 export default async function handler(
